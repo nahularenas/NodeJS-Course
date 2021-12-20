82f8a3db22017a3f4451015c3f0c6927a57284ae
Nahuel Larenas <nahularenas2@gmail.com>
2021-12-20 15:00:05 -0300

    File uploading

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,11 +6,20 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
+
+// Include other resource routers
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
+// Re-route into other resorce routers
+router.use('/:bootcampId/courses', courseRouter);
+
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 
